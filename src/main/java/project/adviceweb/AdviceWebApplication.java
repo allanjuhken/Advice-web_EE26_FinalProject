419813009b45e0013869669161250067a0811d38
allanjuhken <allan.juhken@gmail.com>
2023-07-21 00:56:03 +0300

    Initial commit

---
+++ src/main/java/project/adviceweb/AdviceWebApplication.java
@@ -0,0 +1,13 @@
+package project.adviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdviceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/adviceweb/AdviceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package project.adviceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

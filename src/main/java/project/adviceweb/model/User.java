888ed51e4cdc8d1d23951fa903600093ccdea7e3
kadikristel <kadikristel@hotmail.com>
2023-07-30 08:38:33 +0300

    exceptions

--- src/main/java/project/adviceweb/model/User.java
+++ src/main/java/project/adviceweb/model/User.java
@@ -1,9 +1,13 @@
 package project.adviceweb.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.security.PrivateKey;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,10 +19,14 @@ public class User {
     @GeneratedValue(strategy =
             GenerationType.AUTO)
     private Long userId;
+    //@NotBlank(message = "username can't be empty")
     private String displayedName;
+    //@Email(message = "email format is not correct")
     private String email;
+    //@Size(min = 8, max = 15)
     private String password;
     private String answerProvided;
     private String commentsAdded;
     private String questionAsked;
+    private String role;
 }
--- src/main/java/project/adviceweb/service/UserService.java
+++ src/main/java/project/adviceweb/service/UserService.java
@@ -1,13 +1,10 @@
 package project.adviceweb.service;
 
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import project.adviceweb.dto.UserDto;
 import project.adviceweb.exception.AnswerNotFoundException;
 import project.adviceweb.exception.CommentNotFoundException;
 import project.adviceweb.exception.QuestionNotFoundException;
-import project.adviceweb.exception.UserNotFoundException;
-import project.adviceweb.model.Question;
 import project.adviceweb.model.User;
 import project.adviceweb.repository.UserRepository;
 
@@ -25,26 +22,20 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    @SneakyThrows
     public List<User> findUserByQuestionAsked(String content)
-    throws UserNotFoundException {
-        List<User> user1 = userRepository.findUserByQuestionAsked(content);
+            throws QuestionNotFoundException {
         if (content == null)
             throw new QuestionNotFoundException("Question not found", 4);
         return userRepository.findUserByQuestionAsked(content);
     }
 
-    @SneakyThrows
-    public List<User> findUserByAnswerProvided(String answerProvided) {
-        List<User> user2 = userRepository.findUserByAnswerProvided(answerProvided);
+    public List<User> findUserByAnswerProvided(String answerProvided) throws AnswerNotFoundException {
         if (answerProvided == null)
             throw new AnswerNotFoundException("Answer not found", 1);
         return userRepository.findUserByAnswerProvided(answerProvided);
     }
 
-    @SneakyThrows
-    public List<User> findUserByCommentsAdded(String content) {
-        List<User> user3 = userRepository.findUserByCommentsAdded(content);
+    public List<User> findUserByCommentsAdded(String content) throws CommentNotFoundException {
         if (content == null)
             throw new CommentNotFoundException("Comment not found", 3);
         return userRepository.findUserByCommentsAdded(content);
@@ -56,9 +47,19 @@ public class UserService {
         return toUserDto(savedUser);
     }
 
-    private User toUser(Long id, UserDto userDto) {
+    public void updateUser(UserDto userDto) {
+        User user = userRepository.findById(userDto.getId()).orElse(null);
+        if (user != null) {
+            user.setDisplayedName(userDto.getDisplayedName());
+            user.setEmail(userDto.getEmail());
+            user.setPassword(userDto.getPassword());
+            userRepository.save(user);
+        }
+    }
+
+    private User toUser(Long userId, UserDto userDto) {
         User user = new User();
-        user.setUserId(userDto.getId());
+        user.setUserId(userId);
         user.setEmail(userDto.getEmail());
         user.setPassword(userDto.getPassword());
         user.setDisplayedName(userDto.getDisplayedName());
@@ -69,8 +70,12 @@ public class UserService {
         UserDto savedUserDto = new UserDto();
         savedUserDto.setId(savedUser.getUserId());
         savedUserDto.setDisplayedName(savedUser.getDisplayedName());
-        savedUserDto.setPassword(savedUser.getPassword());
         savedUserDto.setEmail(savedUser.getEmail());
+        savedUserDto.setPassword(savedUser.getPassword());
         return savedUserDto;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }

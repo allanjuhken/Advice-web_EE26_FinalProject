653be5f258506c428a5a415c25b99c51433d4719
kadikristel <kadikristel@hotmail.com>
2023-08-01 10:38:56 +0300

    exceptions

--- src/main/java/project/adviceweb/controller/AdminController.java
+++ src/main/java/project/adviceweb/controller/AdminController.java
@@ -1,17 +1,15 @@
 package project.adviceweb.controller;
 
+import org.hibernate.metamodel.internal.StandardEmbeddableInstantiator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import project.adviceweb.dto.CategoryDto;
 import project.adviceweb.dto.UserDto;
 import project.adviceweb.exception.CategoryNotFoundException;
-import project.adviceweb.model.Category;
-import project.adviceweb.model.Comment;
-import project.adviceweb.model.User;
+import project.adviceweb.exception.QuestionNotFoundException;
+import project.adviceweb.model.*;
 import project.adviceweb.service.*;
 
 import java.util.List;
@@ -32,7 +30,7 @@ public class AdminController {
         this.commentService = commentService;
     }
 
-    @GetMapping("/user")
+    @GetMapping("/user/register-user")
     public String showRegisterPage(ModelMap modelMap) {
         User user = new User();
         modelMap.addAttribute("user", user);
@@ -62,7 +60,7 @@ public class AdminController {
     public String getCategories(final ModelMap modelMap) {
         List<CategoryDto> categoryDtoList = categoryService.getAllCategories();
         modelMap.addAttribute("categoryDtoList", categoryDtoList);
-        return "index";
+        return "create-category";
     }
 
     @GetMapping(value = "/admin/category/create")
@@ -85,7 +83,7 @@ public class AdminController {
         }
     }
 
-    @GetMapping("admin/comment/create")
+    @GetMapping("/admin/comment/create")
     public String showCreateCommentForm(ModelMap modelMap) {
         Comment comment = new Comment();
         modelMap.addAttribute("comment", comment);
@@ -93,9 +91,96 @@ public class AdminController {
     }
 
     @PostMapping("/admin/comment")
-    public String createComment(@ModelAttribute("comment") Comment comment) {
-        commentService.save(comment);
+    public String createComment(@ModelAttribute("comment")
+                                    Comment comment) {
+        commentService.saveComment(comment);
         return "redirect:/";
+    }
+
+    @GetMapping("/admin/question/create")
+    public String showCreateQuestionForm(ModelMap modelMap) {
+        Question question = new Question();
+        modelMap.addAttribute("question", question);
+        return "create-question";
+    }
+
+    @PostMapping("/admin/question")
+    public String createQuestion(@ModelAttribute("question") Question question) {
+        questionService.save(question);
+        return "redirect:/";
+    }
+
+    @GetMapping("/admin/answer/create")
+    public String showCreateAnswerForm(ModelMap modelMap) {
+        Answer answer = new Answer();
+        modelMap.addAttribute("answer", answer);
+        return "create-answer";
+    }
+
+    @PostMapping("/admin/answer")
+    public String createAnswer(@ModelAttribute("answer") Answer answer) {
+        answerService.save(answer);
+        return "redirect:/";
+    }
+
+    @GetMapping("/admin/category-computer/create")
+    public String showCreateCategoryComputerForm(ModelMap modelMap) {
+        Category category = new Category();
+        modelMap.addAttribute("category", category);
+        return "category-computer";
+    }
+
+    @PostMapping("/admin/category-computer")
+    public String createCategoryComputer(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        return "redirect:/";
+    }
+
+    @GetMapping("/admin/category-cooking/create")
+    public String showCreateCategoryCookingForm(ModelMap modelMap) {
+        Category category = new Category();
+        modelMap.addAttribute("category", category);
+        return "category-cooking";
+    }
+
+    @PostMapping("/admin/category-cooking")
+    public String createCategoryCooking(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        return "redirect:/";
+    }
+
+    @GetMapping("/admin/category-health/create")
+    public String showCreateCategoryHealthForm(ModelMap modelMap) {
+        Category category = new Category();
+        modelMap.addAttribute("category", category);
+        return "category-health";
+    }
+
+    @PostMapping("/admin/category-health")
+    public String createCategoryHealth(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        return "redirect:/";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "query", required = false) String query, Model model) {
+        List<CategoryDto> searchResults = categoryService.searchCategories(query);
+        model.addAttribute("searchResults", searchResults);
+        return "search-results";
+    }
+
+    @GetMapping("/category/{categoryName}")
+    public String browseQuestionByCategory(@PathVariable String categoryName, Model model) throws QuestionNotFoundException {
+        List<Question> questions = questionService.findQuestionByCategory(categoryName);
+        model.addAttribute("questions", questions);
+        return "questions";
+    }
+
+    @GetMapping("/question/{questionId}")
+    public String viewQuestion(@PathVariable Long questionId, Model model) throws QuestionNotFoundException {
+        Question question = questionService.findQuestionById(questionId);
+        model.addAttribute("question", question);
+        return "create-question";
     }
 
     
